e99e0e1a72b26884c7413bbac53987b76955e2b8
biancamaxine <bianca.maxine.dev@gmail.com>
2023-04-22 23:16:53 -0400

    feature: ADD httpExeptionMiddleware

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 
+import httpExeptionMiddleware from './middlewares/http-exeption.middleware';
 import index from './routes/index/index.routes';
 import projects from './routes/projects/projects.routes';
 
@@ -11,7 +12,9 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
-app.use('/', index)
-app.use('/projects', projects)
+app.use('/', index);
+app.use('/projects', projects);
+
+app.use(httpExeptionMiddleware);
 
 export default app;
---
+++ src/app/middlewares/http-exeption.middleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+
+import HttpExeption from '../helpers/http-exeption.helper';
+
+export default function httpExeptionMiddleware(
+  err: Error & HttpExeption,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const statusCode = err.statusCode ?? 500;
+  const message = err.statusCode ? err.message : 'Internal server error.'
+
+  return res.status(statusCode).json({ message });
+}
